8a2ec97eed4f01be53246651ae7e5ff1ad80a9d8
Vivek Singh <vivekbrcd@brocade.com>
2018-04-22 10:15:10 +0530

    Added support to filter for solditem also.

--- dhs/dhs/static/js/dhs.sale.js
+++ dhs/dhs/static/js/dhs.sale.js
@@ -16,6 +16,24 @@ function product_search() {
   }
 }
 
+function sold_product_search() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("soldInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("soldTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 
 
 /*
